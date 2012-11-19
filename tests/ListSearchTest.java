5832bf5549205b6278bab58afaa90cecdc3d3d4a
fdezfuentes <git@fdezfuentes.com>
2012-11-19 14:37:09 +0100

    Added new tests for lastIndexOf()

--- tests/ListSearchTest.java
+++ tests/ListSearchTest.java
@@ -68,6 +68,35 @@ public class ListSearchTest {
 		assertTrue(list.indexOf(new Integer(1)) == 0);
 	}
 	
+	@Test
+	public void lastIndexOfAnUnexistingElement() {
+		assertTrue(list.isEmpty());
+		assertTrue(list.lastIndexOf(new Integer(9)) == -1);
+		
+		list.add(new Integer(1)); // list = [1]
+		list.add(new Integer(2)); // list = [1, 2]
+		list.add(new Integer(3)); // list = [1, 2, 3]
+		list.add(new Integer(4)); // list = [1, 2, 3, 4]
+		assertTrue(list.size() == 4);
+		assertTrue(list.lastIndexOf(new Integer(9)) == -1);
+	}
+	
+	@Test
+	public void lastIndexOfAnExistingElement() {
+		assertTrue(list.isEmpty());
+		list.add(new Integer(1)); // list = [1]
+		list.add(new Integer(3)); // list = [1, 3]
+		list.add(new Integer(2)); // list = [1, 3, 2]
+		list.add(new Integer(3)); // list = [1, 3, 2, 3]
+		list.add(new Integer(4)); // list = [1, 3, 2, 3, 4]
+		list.add(new Integer(4)); // list = [1, 3, 2, 3, 4, 4]
+		assertTrue(list.size() == 6);
+		assertTrue(list.lastIndexOf(new Integer(4)) == 5);
+		assertTrue(list.lastIndexOf(new Integer(3)) == 3);
+		assertTrue(list.lastIndexOf(new Integer(2)) == 2);
+		assertTrue(list.lastIndexOf(new Integer(1)) == 0);
+	}
+	
 	@After
 	public void finish() {
 		list = null;
